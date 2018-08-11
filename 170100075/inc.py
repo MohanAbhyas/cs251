964681bcedbf824c4aa49e4c552a72089ba210ed
tusharag121099 <tusharag121099@gmail.com>
2018-08-11 22:30:41 +0530

    1,7 completed

--- 170100075/inc.py
+++ 170100075/inc.py
@@ -6,9 +6,9 @@ def ang_to_vec (ang):
 	vector=np.column_stack((x,y))
 	return vector
 
-def ang_to_vec (vec):
-	a=vec(...,0)
-	b=vec(...,1)
+def vec_to_ang (vec):
+	a=vec[...,0]
+	b=vec[...,1]
 	rad=np.arctan2(b,a) 		#arctan2(x,y) angle for computes x/y
 	degree=np.degrees(rad)
 	return degree
---
+++ 170100075/use_inc.py
@@ -0,0 +1,20 @@
+from inc import *
+import sys 
+k=sys.argv
+input_file=k[1]
+output_file=k[2]
+flag=int(k[3])
+try:
+	data=np.loadtxt(input_file,delimiter=",")
+	
+	if (data.ndim==2 and flag==1):
+		out=np.array(vec_to_ang(data))
+		np.savetxt(output_file,out,delimiter=",")
+	elif (data.ndim==1 and flag==0):
+		print("0")
+		out=ang_to_vec(data)
+		np.savetxt(output_file,out,delimiter=",")
+	else:
+		raise ValueError
+except Exception as e:
+	exit(1)
