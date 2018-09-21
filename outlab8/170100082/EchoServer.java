e34e079e69e869c14181abeecd5cf3cb0f575d48
Arnabjana1999 <39020660+Arnabjana1999@users.noreply.github.com>
2018-09-21 13:31:41 +0530

    P2 Echoserver and Client(Checking) added
    Client.java takes the client_message as Command-Line-Argument.

---
+++ outlab8/170100082/EchoServer.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+ 
+public class EchoServer
+{
+ 
+    private static Socket socket;
+ 
+    public static void main(String[] args)
+    {
+        try
+        {
+ 	    int port = 25000;
+            ServerSocket serverSocket = new ServerSocket(port);
+	    String server_quote="";
+	    for(int i=0;i<args.length-1;i++)
+   	    {
+             server_quote+=args[i]+" ";
+            }
+	    server_quote+=args[args.length-1];
+            while(true)
+            {
+                socket = serverSocket.accept();
+                InputStream is = socket.getInputStream();
+                InputStreamReader isr = new InputStreamReader(is);
+                BufferedReader br = new BufferedReader(isr);
+                String client_message = br.readLine();
+                System.out.println(client_message);
+                String returnMessage;
+                returnMessage = client_message + server_quote + "\n";
+
+                OutputStream os = socket.getOutputStream();
+                OutputStreamWriter osw = new OutputStreamWriter(os);
+                BufferedWriter bw = new BufferedWriter(osw);
+                bw.write(returnMessage);
+                bw.flush();
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                socket.close();
+            }
+            catch(Exception e){}
+        }
+    }
+}
