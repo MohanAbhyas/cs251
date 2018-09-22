648e5287442fbc95d3beeebcfdacc2588b660eab
TUSHAR AGRAWAL <tusharagrawal@cse.iitb.ac.in>
2018-09-22 16:04:55 +0530

    adds testcase generator

---
+++ outlab8/170100075/writetestcase.java
@@ -0,0 +1,28 @@
+import java.io.FileWriter; 
+import java.util.Scanner;
+public class writetestcase{
+	public static void main(String args[])
+	{ 
+		Scanner sc= new Scanner(System.in);
+		System.out.println("n");
+		int n=sc.nextInt();
+        
+		System.out.println("p");
+        int p=sc.nextInt();
+
+		System.out.println("testcasefilenumber");
+        int f=sc.nextInt();
+
+		try{    
+		   FileWriter fw=new FileWriter("testcase"+f+".txt");
+		   fw.write(5+" "+8+"\n");
+		   //fw.newLine();
+		   for (int i=1 ;i<Math.pow(8,5)+1 ; ++i) {
+			fw.write(i+" ");
+		}    
+		   fw.close();    
+		  }
+		  catch(Exception e){System.out.println(e);}    
+          System.out.println("Success...");    
+     }    
+	}
